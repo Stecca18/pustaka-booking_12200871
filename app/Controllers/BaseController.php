08ce42a838684b34ff3d09de8cbb2f6ea3a40962
Stecca18 <90846374+Stecca18@users.noreply.github.com>
2021-10-04 12:31:12 +0700

    Tugas Pertemuan 3

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -7,6 +7,7 @@ use CodeIgniter\HTTP\CLIRequest;
 use CodeIgniter\HTTP\IncomingRequest;
 use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
+use CodeIgniter\Session\Session;
 use Psr\Log\LoggerInterface;
 
 /**
@@ -37,6 +38,11 @@ class BaseController extends Controller
      */
     protected $helpers = [];
 
+
+    /**
+     * @var Session
+     */
+    protected $session;
     /**
      * Constructor.
      */
@@ -47,6 +53,6 @@ class BaseController extends Controller
 
         // Preload any models, libraries, etc, here.
 
-        // E.g.: $this->session = \Config\Services::session();
+        $this->session = \Config\Services::session();
     }
 }
